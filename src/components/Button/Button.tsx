0351853ffe5462bb386caa5d91caba5ac0867077
Julia B <julia.bohlin@hyperisland.se>
2026-04-23 11:41:09 +0200

    add button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,25 @@
+import styles from './Button.module.css'
+
+interface ButtonProps {
+  label: string
+  variant?: 'primary' | 'secondary' | 'outlined'
+  disabled?: boolean
+  onClick: () => void
+}
+
+export const Button = ({
+  label,
+  variant = 'primary',
+  disabled = false,
+  onClick,
+}: ButtonProps) => {
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={`${styles.button} ${styles[variant]}`}
+    >
+      {label}
+    </button>
+  )
+}
