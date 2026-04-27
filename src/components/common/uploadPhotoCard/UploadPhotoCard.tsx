c1b3622060a821bbaee851bc99ca89fae23efd47
Julia B <julia.bohlin@hyperisland.se>
2026-04-27 15:57:19 +0200

    add chose file

--- src/components/common/uploadPhotoCard/UploadPhotoCard.tsx
+++ src/components/common/uploadPhotoCard/UploadPhotoCard.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import styles from './UploadPhotoCard.module.css'
-import { Image as ImageIcon, LoaderCircle, Check } from 'lucide-react'
+import { Image as ImageIcon, LoaderCircle, Check, X } from 'lucide-react'
 import { useStory } from '@/context/StoryContext'
+import Image from 'next/image'
 
 type CardState = 'default' | 'loading' | 'selected'
 
@@ -13,18 +14,50 @@ export function UploadPhotoCard({
     initialState = 'default',
 }: UploadPhotoCardProps) {
     const [cardState, setCardState] = useState<CardState>(initialState)
+    const [selectedFile, setSelectedFile] = useState<HTMLInputElement | null>(
+        null,
+    )
     const { storyData, updateStoryData } = useStory()
 
-    function openLibrary(): void {
-        console.log('choose pic')
+    const handleClick = () => {
+        selectedFile?.click()
+    }
+
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0]
+        if (file) {
+            setCardState('loading')
+            setTimeout(() => {
+                updateStoryData({ photo: file })
+                setCardState('selected')
+            }, 1000)
+        }
+    }
+
+    const resetUpload = () => {
+        setCardState('loading')
+        setTimeout(() => {
+            updateStoryData({ photo: null })
+            setCardState('default')
+        }, 1000)
     }
 
     return (
-        <div className={`${styles.root} ${styles[cardState]}`}>
+        <div
+            className={`${styles.root} ${styles[cardState]}`}
+            onClick={cardState === 'default' ? handleClick : undefined}
+        >
+            <input
+                ref={setSelectedFile}
+                type="file"
+                accept="image/*"
+                style={{ display: 'none' }}
+                onChange={handleFileChange}
+            />
             {cardState === 'default' && (
                 <>
                     <div className={styles.iconWrapper}>
-                        <ImageIcon onClick={() => openLibrary} />
+                        <ImageIcon />
                     </div>
                     <div className={styles.textContainer}>
                         <p>Upload a photo</p>
@@ -41,6 +74,8 @@ export function UploadPhotoCard({
                         <Check />
                     </div>
                     <p>Your photo is uploaded</p>
+                    <X onClick={resetUpload} />
+                    {/* <Image src={JSON.stringify(storyData.photo)} alt="test" /> */}
                 </>
             )}
         </div>
