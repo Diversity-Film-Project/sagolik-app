62bac79e1d04e66eb6d00121c5395d46aa438305
Anna Baidikova <annabaidikova92@gmail.com>
2026-04-27 07:54:11 +0200

    style adjustments

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -7,6 +7,96 @@ export default function UploadPhotoPage() {
     return (
         <PageLayout currentStep={1} href="/upload">
             <h1>Upload photo content</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+                nam consectetur sunt voluptates doloribus pariatur quia ad saepe
+                nemo! Perspiciatis consequuntur sequi vel, expedita temporibus
+                dicta rem! Eius, recusandae nisi! Lorem ipsum dolor sit, amet
+                consectetur adipisicing elit. Error corporis libero possimus,
+                nulla harum nesciunt sapiente deserunt debitis sunt nostrum
+                veniam ipsam quibusdam eos ea suscipit maxime voluptatibus cum
+                aspernatur!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+                nam consectetur sunt voluptates doloribus pariatur quia ad saepe
+                nemo! Perspiciatis consequuntur sequi vel, expedita temporibus
+                dicta rem! Eius, recusandae nisi! Lorem ipsum dolor sit, amet
+                consectetur adipisicing elit. Error corporis libero possimus,
+                nulla harum nesciunt sapiente deserunt debitis sunt nostrum
+                veniam ipsam quibusdam eos ea suscipit maxime voluptatibus cum
+                aspernatur!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+                nam consectetur sunt voluptates doloribus pariatur quia ad saepe
+                nemo! Perspiciatis consequuntur sequi vel, expedita temporibus
+                dicta rem! Eius, recusandae nisi! Lorem ipsum dolor sit, amet
+                consectetur adipisicing elit. Error corporis libero possimus,
+                nulla harum nesciunt sapiente deserunt debitis sunt nostrum
+                veniam ipsam quibusdam eos ea suscipit maxime voluptatibus cum
+                aspernatur!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+                nam consectetur sunt voluptates doloribus pariatur quia ad saepe
+                nemo! Perspiciatis consequuntur sequi vel, expedita temporibus
+                dicta rem! Eius, recusandae nisi! Lorem ipsum dolor sit, amet
+                consectetur adipisicing elit. Error corporis libero possimus,
+                nulla harum nesciunt sapiente deserunt debitis sunt nostrum
+                veniam ipsam quibusdam eos ea suscipit maxime voluptatibus cum
+                aspernatur!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+                nam consectetur sunt voluptates doloribus pariatur quia ad saepe
+                nemo! Perspiciatis consequuntur sequi vel, expedita temporibus
+                dicta rem! Eius, recusandae nisi! Lorem ipsum dolor sit, amet
+                consectetur adipisicing elit. Error corporis libero possimus,
+                nulla harum nesciunt sapiente deserunt debitis sunt nostrum
+                veniam ipsam quibusdam eos ea suscipit maxime voluptatibus cum
+                aspernatur!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+                nam consectetur sunt voluptates doloribus pariatur quia ad saepe
+                nemo! Perspiciatis consequuntur sequi vel, expedita temporibus
+                dicta rem! Eius, recusandae nisi! Lorem ipsum dolor sit, amet
+                consectetur adipisicing elit. Error corporis libero possimus,
+                nulla harum nesciunt sapiente deserunt debitis sunt nostrum
+                veniam ipsam quibusdam eos ea suscipit maxime voluptatibus cum
+                aspernatur!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+                nam consectetur sunt voluptates doloribus pariatur quia ad saepe
+                nemo! Perspiciatis consequuntur sequi vel, expedita temporibus
+                dicta rem! Eius, recusandae nisi! Lorem ipsum dolor sit, amet
+                consectetur adipisicing elit. Error corporis libero possimus,
+                nulla harum nesciunt sapiente deserunt debitis sunt nostrum
+                veniam ipsam quibusdam eos ea suscipit maxime voluptatibus cum
+                aspernatur!
+            </p>{' '}
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+                nam consectetur sunt voluptates doloribus pariatur quia ad saepe
+                nemo! Perspiciatis consequuntur sequi vel, expedita temporibus
+                dicta rem! Eius, recusandae nisi! Lorem ipsum dolor sit, amet
+                consectetur adipisicing elit. Error corporis libero possimus,
+                nulla harum nesciunt sapiente deserunt debitis sunt nostrum
+                veniam ipsam quibusdam eos ea suscipit maxime voluptatibus cum
+                aspernatur!
+            </p>{' '}
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+                nam consectetur sunt voluptates doloribus pariatur quia ad saepe
+                nemo! Perspiciatis consequuntur sequi vel, expedita temporibus
+                dicta rem! Eius, recusandae nisi! Lorem ipsum dolor sit, amet
+                consectetur adipisicing elit. Error corporis libero possimus,
+                nulla harum nesciunt sapiente deserunt debitis sunt nostrum
+                veniam ipsam quibusdam eos ea suscipit maxime voluptatibus cum
+                aspernatur!
+            </p>
         </PageLayout>
     )
 }
