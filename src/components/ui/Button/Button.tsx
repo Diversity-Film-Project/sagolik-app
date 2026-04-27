be6d1aaa8181a1b0b258c369bf920d9fa2e1c40c
Julia B <julia.bohlin@hyperisland.se>
2026-04-27 10:19:37 +0200

    button css

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,25 +1,25 @@
-import styles from "./Button.module.css";
+import styles from './Button.module.css'
 
 interface buttonProps {
-  label: string;
-  variant?: "primary" | "secondary" | "outlined";
-  disabled?: boolean;
-  onClick?: () => void
+    label: string
+    variant?: 'primary' | 'secondary' | 'outlined'
+    disabled?: boolean
+    onClick?: () => void
 }
 
 export const Button = ({
-  label,
-  variant = "primary",
-  disabled = false,
-  onClick,
+    label,
+    variant = 'primary',
+    disabled = false,
+    onClick,
 }: buttonProps) => {
-  return (
-    <button
-      disabled={disabled}
-      onClick={onClick}
-      className={`${styles.root} ${styles[variant]}`}
-    >
-      {label}
-    </button>
-  );
-};
+    return (
+        <button
+            disabled={disabled}
+            onClick={onClick}
+            className={`${styles.root} ${variant ? styles[variant] : ''}`}
+        >
+            {label}
+        </button>
+    )
+}
