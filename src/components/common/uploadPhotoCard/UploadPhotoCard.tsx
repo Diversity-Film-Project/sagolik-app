0add5a664e309a3efdd3c9c9c85b16aedc06a7ab
Julia B <julia.bohlin@hyperisland.se>
2026-04-27 13:56:06 +0200

    set up component structure

--- src/components/common/uploadPhotoCard/UploadPhotoCard.tsx
+++ src/components/common/uploadPhotoCard/UploadPhotoCard.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import styles from './UploadPhotoCard.module.css'
 
+type CardState = 'default' | 'loading' | 'selected'
+
 export function UploadPhotoCard({}) {
+    const [cardState, setCardState] = useState<CardState>('default')
+
     return (
-        <div>
+        <div className={styles.root}>
             <p>photo card</p>
+            {cardState === 'default' && <p>Upload photo</p>}
+            {cardState === 'loading' && <p>Loading..</p>}
+            {cardState === 'selected' && <p>Selected photo</p>}
         </div>
     )
 }
