d903e21301de60327e647a02ca6204bb87a30e4f
Julia B <julia.bohlin@hyperisland.se>
2026-04-27 12:07:53 +0200

    add story context

---
+++ src/context/StoryContext.tsx
@@ -0,0 +1,38 @@
+import { createContext, useState } from 'react'
+
+interface StoryData {
+    photo: File | null
+    characterName: string
+    storyTheme: string
+    sidekick: string
+}
+
+interface StoryContextType {
+    storyData: StoryData
+    updateStoryData: (data: Partial<StoryData>) => void
+}
+
+type StoryProviderProps = {
+    children: React.ReactNode
+}
+
+const StoryContext = createContext<StoryContextType | null>(null)
+
+const StoryProvider = ({ children }: StoryProviderProps) => {
+    const [storyData, setStoryData] = useState<StoryData>({
+        photo: null,
+        characterName: '',
+        storyTheme: '',
+        sidekick: '',
+    })
+
+    const updateStoryData = (data: Partial<StoryData>) => {
+        setStoryData((prev) => ({ ...prev, ...data }))
+    }
+
+    return (
+        <StoryContext.Provider value={{ storyData, updateStoryData }}>
+            {children}
+        </StoryContext.Provider>
+    )
+}
