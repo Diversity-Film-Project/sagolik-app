7560fc51d4d65c14b17ef0b5c86f3cfb7d3f0671
Julia B <julia.bohlin@hyperisland.se>
2026-04-28 13:56:59 +0200

    single card component

---
+++ src/components/common/IconCard/IconCard.tsx
@@ -0,0 +1,28 @@
+import styles from './IconCard.module.css'
+
+interface IconCardProps {
+    icon: React.ReactElement
+    label: string
+    variant: 'default' | 'selected'
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+export function IconCard({
+    icon,
+    label = 'any theme',
+    variant = 'default',
+    onClick,
+}: IconCardProps) {
+    return (
+        <>
+            <h1>icon card</h1>
+            <button
+                className={`${styles.cardContainer} ${styles[variant]}`}
+                onClick={onClick}
+            >
+                {icon}
+                {label}
+            </button>
+        </>
+    )
+}
