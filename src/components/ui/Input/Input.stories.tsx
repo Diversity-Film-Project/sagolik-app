e5f463acd6b5d500a09474322c0c817c5ad39e85
simmanomar <omar.ssimman@gmail.com>
2026-05-01 14:18:59 +0200

    input component

---
+++ src/components/ui/Input/Input.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { Input } from './Input'
+
+const meta: Meta<typeof Input> = {
+    title: 'Components/Input',
+    component: Input,
+
+    args: {
+        value: '',
+        placeholder: 'Placeholder',
+        onChange: () => {},
+    },
+}
+
+export default meta
+
+type Story = StoryObj<typeof Input>
+
+export const Primary: Story = {
+    args: {
+        value: '',
+        placeholder: 'Character name',
+        variant: 'primary',
+    },
+}
+
+export const Secondary: Story = {
+    args: {
+        value: '',
+        placeholder: 'Input field',
+        variant: 'secondary',
+    },
+}
