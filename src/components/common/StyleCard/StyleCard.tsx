4378a48d34e903b35645da870974ecd3a1df1a16
Julia B <julia.bohlin@hyperisland.se>
2026-05-07 11:46:30 +0200

    merge main

--- src/components/common/StyleCard/StyleCard.tsx
+++ src/components/common/StyleCard/StyleCard.tsx
@@ -2,17 +2,20 @@ import styles from './StyleCard.module.css'
 
 interface StyleCardProps {
     style: string
-    // isSelected?: boolean
+    isSelected?: boolean
     onClick?: () => void
 }
 
 export function StyleCard({
     style,
-    // isSelected = false,
+    isSelected = false,
     onClick,
 }: StyleCardProps) {
     return (
-        <div onClick={onClick} className={styles.container}>
+        <div
+            onClick={onClick}
+            className={`${styles.container} ${styles[isSelected ? 'selected' : 'default']}`}
+        >
             {style}
         </div>
     )
