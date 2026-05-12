9158ada24ee9980df83c52ee35bfe621f4af778e
Anna Baidikova <annabaidikova92@gmail.com>
2026-05-12 22:59:49 +0200

    bug fix: download on ios

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -185,6 +185,13 @@ export default function ResultPage() {
     const handleDownload = async () => {
         if (downloading) return
         setDownloading(true)
+
+        if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+            window.open(videoUrl, '_blank')
+            setDownloading(false)
+            return
+        }
+
         try {
             await downloadVideo(
                 videoUrl,
